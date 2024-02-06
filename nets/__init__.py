e2d1b5bd331bb2b1addec0208187d666ef1a6106
Kevin Tang <103300322+XinKai-Tang@users.noreply.github.com>
2024-02-06 15:24:22 +0800

    update nets

---
+++ nets/__init__.py
@@ -0,0 +1,7 @@
+# (1) Detection Networks:
+from .NoduleNet.nodule_net import NoduleNet
+from .SANet.sanet import SANet
+
+# (2) Segmentation Networks:
+from .SwinUNETR.swin_unetr import SwinUNETR
+from .UXNet.uxnet3d import UXNET
